3eb31975465f01e9d7a4887014efd53c014b3b3a
João Passareli <joaopassareli@gmail.com>
2023-03-29 20:58:16 -0300

    Criando o controller Api/Pet

---
+++ app/Http/Controllers/Api/PetController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Pet;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PetController extends Controller
+{
+    public function index()
+    {
+        return Pet::all();
+    }
+
+    public function store()
+    {}
+
+    public function update()
+    {}
+
+    public function destroy()
+    {}
+
+
+    /**
+     * Show a pet from a id given
+     *
+     * @param  int $pet with the pet Id
+     * @return Pet with all the data from the Pet where the id was Given
+     */
+    public function show(int $pet)
+    {
+        $pet = Pet::whereId($pet)
+            ->with('seasons.episodes')
+            ->first();
+        return $pet;
+    }
+}
